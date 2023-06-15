f76b139bf9e9a52b9a5e5649deb8bfc771e65d3e
Fithian_Justin <fithj492@hsd.k12.or.us>
2023-06-15 12:40:22 -0700

    I upgraded str and dex

--- rpg_project/src/main/java/com/rpg_project/Character.java
+++ rpg_project/src/main/java/com/rpg_project/Character.java
@@ -1,13 +1,43 @@
 package com.rpg_project;
+import java.util.ArrayList;
 
 public class Character {
+
+    //add stats
     private String name;
     private int strength;
     private int dexterity;
+    private int strengthModifier = DM.getStatsModifier(strength);
+    private ArrayList<String> inventory = new ArrayList<String>();
+    private int maxInventorySize = 5;
+    private int constitution;
+
+    public static void main(String[] args) {
+        System.out.println("I test many many class ");
+    }
 
     // instance methods
     public Character(String name) {
         this.name = name;
+    }
+
+    public int getConstitution() {
+        return constitution;
+    }
+
+    public void setConstitution(int constitution) {
+        this.constitution = constitution;
+    }
+
+    
+    public ArrayList<String> getInventory() {
+        return inventory;
+    }
+
+    public void addItem(String item) {
+        // Don't allow the inventory to grow larger than a set amount
+        // hint .size() 
+        this.inventory.add(item);
     }
 
     // getters and setters
@@ -24,6 +54,10 @@ public class Character {
         this.strength = strength;
     }
 
+    public void addStrength(int amount) {
+        this.strength += amount;
+    }
+
     public int getDexterity() {
         return dexterity;
     }
@@ -32,10 +66,13 @@ public class Character {
         this.dexterity = dexterity;
     }
 
+    // toString override
     public String toString() {
-        String output = "Character Name: " + name;
-        output += "\nStrength: " + strength;
-        output += "\nDexterity: " + dexterity;
+        String output = "Character\n";
+        output += "Name: " + this.name;
+        output += "\nStrength: " + this.strength;
+        output += "\nDexterity: " + this.dexterity;
+        output += "\nConstitution: " + this.constitution;
         return output;
     }
 }
